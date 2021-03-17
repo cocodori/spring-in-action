48bb83ce49a7e31d0e38d01ef214f4fbb0cf358b
cocodori <moradulee@gmail.com>
2021-03-17 14:43:16 +0900

    ch03 : 도메인 객체 JPA 애노테이션 추

--- src/main/java/tacos/domain/Taco.java
+++ src/main/java/tacos/domain/Taco.java
@@ -2,13 +2,16 @@ package tacos.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date createdAt;
 
@@ -16,6 +19,12 @@ public class Taco {
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
+    @ManyToOne(targetEntity = Ingredient.class)
     @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createAt() {
+        this.createdAt = new Date();
+    }
 }
