6345202bad4b0af623bb5a6ad1dd6fc05ed595ab
cocodori <moradulee@gmail.com>
2021-03-17 18:06:31 +0900

    ch04: login page 만들기

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -25,11 +25,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/design", "/orders")
+                .antMatchers("/design", ".orders")
                 .access("hasRole('ROLE_USER')")
-                .antMatchers("/", "/**").access("permitAll")
+                .antMatchers("/", "/**")
+                .access("hasRole('permitAll')")
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
     }
 
     @Override
